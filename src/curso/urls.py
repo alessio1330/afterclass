d6972e2e6ddaeffe7b8056683d9c91ca4fc839be
Alessio Alayo <alessio@alayo.com>
2024-12-12 15:06:29 -0500

    Formularios para los modelos

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 
-from .views import about, index
+from .views import (
+    about,
+    index,
+    curso_list,
+    comision_list,
+    alumno_list,
+    curso_create,
+    comision_create,
+    alumno_create,
+)
 
 app_name = "curso"
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
-    ]
+    path("curso/list/", curso_list, name="curso_list"),
+    path("curso/create/", curso_create, name="curso_create"),
+    path("comision/list/", comision_list, name="comision_list"),
+    path("comision/create/", comision_create, name="comision_create"),
+    path("alumno/list", alumno_list, name="alumno_list"),
+    path("alumno/create/", alumno_create, name="alumno_create"),
+]
